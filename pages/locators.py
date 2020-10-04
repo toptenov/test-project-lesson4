4b559ec8025cf74b621bafa104183b26537cd9e8
toptenov <toptenov@gmail.com>
2020-10-04 22:03:56 +0300

    Добавил class LoginPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,9 @@
 from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
