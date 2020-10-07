0e24d126357ef48127c3d8b223681d6ef8bfb7ba
toptenov <toptenov@gmail.com>
2020-10-07 21:54:38 +0300

    Добавил is_text_in_element_correct

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 from selenium.common.exceptions import NoSuchElementException
+import math
 
 class BasePage():
 
@@ -16,3 +17,26 @@ class BasePage():
 	    except (NoSuchElementException):
 	        return False
 	    return True
+
+    def is_text_in_element_correct(self, how, what, text):
+        text_in_element = self.browser.find_element(how, what).text
+        print (f"text_in_element = {text_in_element}, text = {text}")
+        try:
+            text_in_element == text
+        except:
+            return False
+        return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
