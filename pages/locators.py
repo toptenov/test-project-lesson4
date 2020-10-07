224d204f5ad859243f52aa41c8fc2c3e07fee025
toptenov <toptenov@gmail.com>
2020-10-07 21:53:34 +0300

    Добавил class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+	ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket")
+	ADDED_BOOK_NAME = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon.alert-success.fade.in div.alertinner strong")
+	BOOK_PRICE_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon.alert-info.fade.in div.alertinner strong")
