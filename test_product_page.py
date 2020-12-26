e61ef7725126bba309470687426d8b93acbaa56b
toptenov <toptenov@gmail.com>
2020-12-26 15:41:37 +0300

    В test_product_page.py добавил test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from pages.product_page import ProductPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 import time # Можно удалить
 import pytest
 
@@ -60,3 +61,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.should_be_login_link()
     log_page = LoginPage(browser, browser.current_url)
     log_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open() # открываем страницу
+    page.go_to_basket_page() # выполняем метод страницы - переходим на страницу логина
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_products_on_basket_page()
+    basket_page.should_be_text_of_empty_basket()
